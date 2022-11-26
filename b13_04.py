f8eab0f7dc5259358e158e547c6b341f9ccecf87
E-taku <tkymtk8270@gmail.com>
2022-11-26 20:48:13 +0900

    diligence on 11/26

---
+++ b13_04.py
@@ -0,0 +1,62 @@
+N,K = map(int,input().split())
+
+A = list(map(int,input().split()))
+
+
+class SuckingMethod:
+    def choose_pairs_two_integers(self,N: int,K: int, A: list):
+        """
+        小さい順に A1,A2,⋯,AN です。
+        異なる 2 つの整数のペアを選ぶ方法は全部で N(N−1)/2 通りあるが、
+        その中で差が K 以下であるような選び方は何通りあるか。
+        """
+        R = [None] * (N)
+        # 尺取法
+        for i in range(N-1):
+            # スタート地点を決める
+            if i == 0:
+                R[0] = 0
+            else:
+                R[i] = R[i-1]
+            # 増やせるとことまで増やす
+            while (R[i] < N - 1) and (A[R[i] + 1] - A[i]) <= K:
+                R[i] += 1
+        ans = 0
+        for i in range(N-1):
+            ans += (R[i] - i)
+        return ans
+
+    def cumulativeSum_choose_pairs_two_integers(self,N: int,K: int, A: list):
+        """
+        i 番目の品物は Ai円です。連続する番号の品物を買う方法
+        その中で差が K 以下であるような選び方は何通りあるか。
+        累積和と尺取法を組み合わせる
+        """
+        S = [0] * (N+1)
+
+        # 累積和
+        for i in range(1,N+1):
+            S[i] = S[i-1] + A[i-1]
+
+        R = [None] * (N+1)
+
+        for i in range(1,N+1):
+            if i == 1:
+                R[i] = 0
+            else:
+                R[i] = R[i-1]
+            
+            while R[i] < N  and (S[R[i]+1]-S[i-1]) <= K:
+                R[i] += 1
+        
+        ans = 0
+        for i in range(1,N+1):
+            ans += R[i]-(i-1)
+        return ans
+
+
+
+sm = SuckingMethod()
+ans = sm.cumulativeSum_choose_pairs_two_integers(N,K,A)
+print(ans)
+
--- b62_03.py
+++ b62_03.py
@@ -1,28 +1,74 @@
+# import sys
+# from collections import defaultdict
+# sys.setrecursionlimit(10 ** 9)
+
+# N,M = map(int,input().split())
+
+# G = defaultdict(list)
+
+# for i in range(M):
+#     a,b = map(int,input().split())
+#     G[a].append(b)
+#     G[b].append(a)
+
+# visited = [False] * (N+1)
+# path = []
+# def dfs( i: int,):
+#     path.append(i)
+#     if i == N:
+#         print(*path)
+#         exit()
+#     visited[i] = True
+#     for next in G[i]:
+#         if visited[next] == False:
+#             dfs(next)
+#     path.pop()
+
+# dfs(1)
+
+
+
+
+
 import sys
 from collections import defaultdict
 sys.setrecursionlimit(10 ** 9)
 
-N,M = map(int,input().split())
+class Single_path():
+    """
+    頂点数 N、辺数 M の連結なグラフが与えられる。 このグラフについて、頂点 1 から頂点 N までの単純パスを一つ出力。
 
-G = defaultdict(list)
+    """
+    def __init__(self):
+        self.path = []
 
-for i in range(M):
-    a,b = map(int,input().split())
-    G[a].append(b)
-    G[b].append(a)
+    def input(self):
+        self.N,self.M = map(int,input().split())
 
-visited = [False] * (N+1)
-path = []
-def dfs( i: int,):
-    path.append(i)
-    if i == N:
-        print(*path)
-        exit()
-    visited[i] = True
-    for next in G[i]:
-        if visited[next] == False:
-            dfs(next)
-    path.pop()
+        self.G = defaultdict(list)
 
-dfs(1)
+        for i in range(self.M):
+            a,b = map(int,input().split())
+            self.G[a].append(b)
+            self.G[b].append(a)
 
+
+
+    def simple_path(self):
+        visited = [False] * (self.N+1)
+        def dfs(self,i: int):
+            self.path.append(i)
+            if i == self.N:
+                print(*self.path)
+                exit()
+                return self.path
+            visited[i] = True
+            for next in self.G[i]:
+                if visited[next] == False:
+                    dfs(self,next)
+            self.path.pop()
+        dfs(self,1)
+
+sp = Single_path()
+sp.input()
+sp.simple_path()
